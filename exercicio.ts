115655daaf1d873bd5d3cf02dd3decd4a1d41a21
YuriDonato <yuridonatosis@gmail.com>
2023-10-03 22:42:18 -0300

    main commit

---
+++ exercicio.ts
@@ -0,0 +1,14 @@
+// Função que recebe dois numeros e retorna sua multiplicação
+function multiplicaNumeros(num1: number, num2: number) {
+    return num1 * num2;
+}
+// Alternativa em Arrow function
+// const multiplicaNumeros = (num1: number, num2: number) => (num1 * num2);
+
+// Função que recebe um nome e retorna uma saudação:
+function dizOi(nome: string) {
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicaNumeros(4, 2));
+console.log(dizOi("Yuri"));
